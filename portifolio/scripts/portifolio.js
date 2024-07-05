bb0df14dd6cfa81573c69ef121a650991f166816
Samuel Johnny <johnnysamuel486@gmail.com>
2024-07-05 03:36:28 -0300

    Projeto antes da criação do layout e da inteligencia do carrosel

---
+++ portifolio/scripts/portifolio.js
@@ -0,0 +1,4 @@
+const cardProjeto = document.getElementsByClassName("caixa-projeto");
+const setaDireita = document.querySelector(".seta-direita");
+
+setaDireita.addEventListener("click", function () {});
