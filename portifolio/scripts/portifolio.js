b471eb7bf3f50c51623325e7b2359efbe5ea02b1
Samuel Johnny <johnnysamuel486@gmail.com>
2024-07-12 22:28:20 -0300

    Pagina principal do portifolio responsiva

--- portifolio/scripts/portifolio.js
+++ portifolio/scripts/portifolio.js
@@ -2,14 +2,33 @@ const containerCard = document.querySelector(".container-card-projeto");
 const container = document.querySelector(".container");
 const setaDireta = document.querySelector(".seta-direita");
 const setaEsquerda = document.querySelector(".seta-esquerda");
+const menuIcone = document.querySelector(".icone-menu");
+const menuLateral = document.querySelector(".menu-lateral");
 
 function nextCard() {
-  containerCard.scrollLeft += containerCard.clientWidth + 16;
+  if (document.documentElement.clientWidth > 767) {
+    containerCard.scrollLeft += containerCard.clientWidth + 16;
+  } else {
+    containerCard.scrollLeft += containerCard.clientWidth;
+  }
 }
 
 function previousCard() {
-  containerCard.scrollLeft -= containerCard.clientWidth + 16;
+  if (document.documentElement.clientWidth > 767) {
+    containerCard.scrollLeft -= containerCard.clientWidth + 16;
+  } else {
+    containerCard.scrollLeft -= containerCard.clientWidth;
+  }
 }
 
+function openMenu() {
+  if (!menuLateral.classList.contains("aberto")) {
+    menuLateral.classList.add("aberto");
+  } else {
+    menuLateral.classList.remove("aberto");
+  }
+}
+
+menuIcone.addEventListener("click", openMenu);
 setaDireta.addEventListener("click", nextCard);
 setaEsquerda.addEventListener("click", previousCard);
