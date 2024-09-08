cf57a0f8e0481740baa2ac1b6974fc530881503e
Samuel Johnny <johnnysamuel486@gmail.com>
2024-09-07 22:46:07 -0300

    retirei as pastas e arquivos da pasta portifolio .implementei a funcionalidade de mudança de imagem e finalizei as legendas dos projetos

--- scripts/portifolio.js
+++ scripts/portifolio.js
@@ -6,6 +6,8 @@ const menuIcone = document.querySelector(".icone-menu");
 const menuLateral = document.querySelector(".menu-lateral");
 const contato = document.querySelector(".contato");
 const sobre = document.querySelector(".item-lista--sobre");
+const abaSuspenca = document.querySelector(".aba-suspenca");
+const img = document.querySelector(".img");
 
 function nextCard() {
   if (document.documentElement.clientWidth > 767) {
@@ -31,10 +33,15 @@ function openMenu() {
   }
 }
 
-function copyEmail(e) {
+function copyEmailAndOpenTabSuspend(e) {
   e.preventDefault();
   const email = "johnnysamuel486@gmail.com";
   navigator.clipboard.writeText(email);
+  abaSuspenca.classList.add("aberta");
+
+  setTimeout(() => {
+    abaSuspenca.classList.remove("aberta");
+  }, "2000");
 }
 
 function aboutScrolling(e) {
@@ -45,8 +52,27 @@ function aboutScrolling(e) {
   });
 }
 
+const imagem1 = "../assets/imagem-1.jpg";
+const imagem2 = "../assets/imagem-2.jpg";
+const imagem3 = "../assets/imagem-3.jpg";
+
+let controle = 1;
+
+function changeImage() {
+  const imagens = [imagem1, imagem2, imagem3];
+
+  if (controle < imagens.length) {
+    img.setAttribute("src", imagens[controle]);
+    controle++;
+  } else {
+    controle = 0;
+  }
+}
+
+setInterval(changeImage, 3000);
+
 menuIcone.addEventListener("click", openMenu);
 setaDireta.addEventListener("click", nextCard);
 setaEsquerda.addEventListener("click", previousCard);
-contato.addEventListener("click", copyEmail);
+contato.addEventListener("click", copyEmailAndOpenTabSuspend);
 sobre.addEventListener("click", aboutScrolling);
